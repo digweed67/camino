7cd805b4202187792699f953bf8f6e5e6c9867b9
digweed67 <124364643+digweed67@users.noreply.github.com>
2025-07-06 14:47:01 +0200

    Add http response codes and error check for cURL and timeout

--- php/getCountryFromCoords.php
+++ php/getCountryFromCoords.php
@@ -5,6 +5,7 @@ $lat = $_POST['lat'] ?? null;
 $lng = $_POST['lng'] ?? null;
 
 if (!$lat || !$lng) {
+    http_response_code(400);    
     echo json_encode(['error' => 'Missing coordinates']);
     exit;
 }
@@ -16,10 +17,17 @@ $url = "https://api.opencagedata.com/geocode/v1/json?q={$lat}+{$lng}&key={$apiKe
 $curl = curl_init();
 curl_setopt_array($curl, [
     CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 8,
 ]);
 
 $response = curl_exec($curl);
+if ($response === false){
+    http_response_code(500);// Internal Server Error
+    $err = curl_error($curl);
+    curl_close($curl);
+    exit(json_encode(['status' => 'error', 'msg' => $err]));
+}
 curl_close($curl);
 
 $data = json_decode($response, true);
@@ -28,5 +36,6 @@ if (isset($data['results'][0]['components']['country_code'])) {
     $countryCode = strtoupper($data['results'][0]['components']['country_code']);
     echo json_encode(['countryCode' => $countryCode]);
 } else {
+    http_response_code(404);
     echo json_encode(['error' => 'Country not found']);
 }
